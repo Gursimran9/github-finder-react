e55d64423dceece9058482feda289258a45eeee0
Gursimran9 <simrangur12345@gmail.com>
2022-01-21 02:05:51 -0500

    Api integrated

--- src/components/layout/Search.js
+++ src/components/layout/Search.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Search = () => {
+const Search = ({ search }) => {
+  const [input, setInput] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    search(input);
+    setInput('');
+  };
   return (
     <div>
-      <form className='form'>
-        <input type='text' name='text' placeholder='Search Users...' />
-        <input type='submit' className='btn btn-dark btn-block' />
+      <form className='form' onSubmit={handleSubmit}>
+        <input
+          type='text'
+          name='text'
+          placeholder='Search Users...'
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <input
+          type='submit'
+          className='btn btn-dark btn-block'
+          value='Search'
+        />
       </form>
     </div>
   );
