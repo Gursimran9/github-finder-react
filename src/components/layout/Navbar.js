2764e2da92cb6d98be879475401f1b68c61e38f6
Gursimran9 <simrangur12345@gmail.com>
2022-01-20 23:08:20 -0500

    Http Request

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,17 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import GitHubIcon from '@mui/icons-material/GitHub';
 
-export class Navbar extends Component {
-  render() {
-    return (
-      <nav className='navbar bg-primary'>
-        <h1>
-          <GitHubIcon />
-          Navbar
-        </h1>
-      </nav>
-    );
-  }
-}
+const Navbar = ({ title }) => {
+  return (
+    <nav className='navbar bg-primary'>
+      <h1>
+        <GitHubIcon />
+        {title}
+      </h1>
+    </nav>
+  );
+};
 
 export default Navbar;
